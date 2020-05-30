0b09fa2e367dda07d6e7e4339faa1f9d8374f49e
Mahmoud Shabana <mshabana5656@gmail.com>
2020-05-30 16:12:55 -0400

    Creating local repo

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -3,6 +3,7 @@
 //  Dicee-iOS13
 //
 //  Created by Angela Yu on 11/06/2019.
+//  Modified by Mahmoud Shabana on 5/29/2020.
 //  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
@@ -10,11 +11,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
+        //WHO          WHAT   VALUE
+    //  diceImageView1.alpha = 0.5
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // When 'Roll' button is pressed, This code block will run.
+        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
 }
 
